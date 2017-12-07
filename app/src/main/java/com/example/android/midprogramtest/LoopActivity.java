9eee3de842d215b4604dcaf119209eb4e2a5c821
Tatiana Moronta <tatianamoronta@ac.c4q.nyc>
2017-12-06 19:19:13 -0500

    initial commit

---
+++ app/src/main/java/com/example/android/midprogramtest/LoopActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.midprogramtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loop);
+    }
+}
