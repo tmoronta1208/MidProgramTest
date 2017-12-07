00a61809137ccc3d09c0a4765cd751552b168d5d
Tatiana Moronta <tatianamoronta@ac.c4q.nyc>
2017-12-06 21:09:12 -0500

    added part02

--- app/src/main/java/com/example/android/midprogramtest/LoginActivity.java
+++ app/src/main/java/com/example/android/midprogramtest/LoginActivity.java
@@ -1,13 +1,62 @@
 package com.example.android.midprogramtest;
 
+import android.app.ListActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button submit;
+    private CheckBox checkBox;
+    private SharedPreferences login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email = (EditText) findViewById(R.id.edit_text1);
+        password = (EditText) findViewById(R.id.edit_text2);
+        submit = (Button) findViewById(R.id.button);
+        checkBox = (CheckBox) findViewById(R.id.checkBox);
+
+        login = getApplicationContext().getSharedPreferences("sharedPrefs", MODE_PRIVATE);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SharedPreferences.Editor editor = login.edit();
+                String checkEmail = email.getText().toString().toLowerCase();
+                String checkPassword = password.getText().toString().toLowerCase();
+
+                if(checkEmail.equals("user@aol.com") && checkPassword.equals("password1234")){
+                    editor.putString("email", email.getText().toString() );
+                    editor.putString("password", password.getText().toString());
+                    editor.putBoolean("isChecked", checkBox.isChecked());
+                    editor.commit();
+
+                }
+                Intent intent = new Intent(LoginActivity.this, ListActivity.class);
+                intent.putExtra("email",email.getText().toString());
+                startActivity(intent);
+            }
+        });
+
     }
 }
