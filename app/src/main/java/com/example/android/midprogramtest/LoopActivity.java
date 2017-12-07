e2a7a51d910716038328ed04589774dccee009b4
Tatiana Moronta <tatianamoronta@ac.c4q.nyc>
2017-12-06 20:09:43 -0500

    added part01

--- app/src/main/java/com/example/android/midprogramtest/LoopActivity.java
+++ app/src/main/java/com/example/android/midprogramtest/LoopActivity.java
@@ -1,13 +1,25 @@
 package com.example.android.midprogramtest;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoopActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loop);
+        textView = (TextView) findViewById(R.id.text_view);
+        Intent intent = new Intent(LoopActivity.this, LoginActivity.class);
+
     }
 }
+
+
+
